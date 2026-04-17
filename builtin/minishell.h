217e12dc1beeaff70d4218a78c76e5a7476a9307
derfl8 <alfred.begou@gmail.com>
2026-04-17 18:04:19 +0200

    add env init, free stack

--- builtin/minishell.h
+++ builtin/minishell.h
@@ -6,7 +6,7 @@
 /*   By: abegou <abegou@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/16 14:22:21 by abegou            #+#    #+#             */
-/*   Updated: 2026/04/16 14:24:14 by abegou           ###   ########.fr       */
+/*   Updated: 2026/04/17 18:03:29 by abegou           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,7 +25,11 @@ typedef struct s_env	{
     
 }					t_env;
 
-int ft_pwd(void);
-int ft_env(char **envp);
+int     ft_pwd(void);
+int     ft_env(t_env *envinfo);
+void	ft_add_back_env(t_env **lst, t_env *new);
+t_env	*ft_new_env(char *envinfo);
+void	ft_free_stack_env(t_env **array);
+t_env   *init_env(char  **envp);
 
 #endif
