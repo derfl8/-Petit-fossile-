b36d57d0ee05f14b31661cab3066a6724363617d
derfl8 <alfred.begou@gmail.com>
2026-04-18 15:17:11 +0200

    handle too many args in env and for pwd in 1st use info in env struct

--- builtin/lst_tools.c
+++ builtin/lst_tools.c
@@ -6,7 +6,7 @@
 /*   By: abegou <abegou@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/16 14:22:01 by abegou            #+#    #+#             */
-/*   Updated: 2026/04/17 18:45:23 by abegou           ###   ########.fr       */
+/*   Updated: 2026/04/18 14:19:34 by abegou           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -51,4 +51,22 @@ void	ft_free_stack_env(t_env **array)
 		free(tmp);
 	}
 	return ;
+}
+
+int	ft_strncmp(const char *s1, const char *s2, size_t n)
+{
+	size_t			i;
+	unsigned char	*s3;
+	unsigned char	*s4;
+
+	i = 0;
+	s3 = (unsigned char *)s1;
+	s4 = (unsigned char *)s2;
+	if (n == 0)
+		return (0);
+	while (s3[i] && s3[i] == s4[i] && i < n - 1)
+	{
+		i++;
+	}
+	return (s3[i] - s4[i]);
 }
