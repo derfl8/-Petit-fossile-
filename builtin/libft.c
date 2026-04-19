7f6dee1966ef4bcbc5c25a9bff19f73c617e94db
derfl8 <alfred.begou@gmail.com>
2026-04-19 20:20:15 +0200

    add makefile & orga of files/folders

---
+++ builtin/libft.c
@@ -0,0 +1,58 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libft.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: abegou <abegou@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/04/19 20:11:57 by abegou            #+#    #+#             */
+/*   Updated: 2026/04/19 20:12:57 by abegou           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../header/minishell.h"
+
+size_t	ft_strlen(const char *s)
+{
+	const char	*tmp = s;
+
+	while (*tmp)
+		tmp++;
+	return (tmp - s);
+}
+
+int	ft_strncmp(const char *s1, const char *s2, size_t n)
+{
+	size_t			i;
+	unsigned char	*s3;
+	unsigned char	*s4;
+
+	i = 0;
+	s3 = (unsigned char *)s1;
+	s4 = (unsigned char *)s2;
+	if (n == 0)
+		return (0);
+	while (s3[i] && s3[i] == s4[i] && i < n - 1)
+	{
+		i++;
+	}
+	return (s3[i] - s4[i]);
+}
+
+char	*ft_strdup(const char *s)
+{
+	char	*d;
+	size_t	i;
+
+	i = 0;
+	d = malloc(ft_strlen(s) * sizeof(char) + 1);
+	if (d == NULL)
+		return (NULL);
+	while (s[i])
+	{
+		d[i] = s[i];
+		i++;
+	}
+	d[i] = '\0';
+	return (d);
+}
