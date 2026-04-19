37d947b3ce23abad26091a31fbf2fde37b8bd978
derfl8 <alfred.begou@gmail.com>
2026-04-19 20:23:31 +0200

    fix keyword and reorga in header

--- header/minishell.h
+++ header/minishell.h
@@ -6,7 +6,7 @@
 /*   By: abegou <abegou@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/16 14:22:21 by abegou            #+#    #+#             */
-/*   Updated: 2026/04/19 19:57:45 by abegou           ###   ########.fr       */
+/*   Updated: 2026/04/19 20:22:24 by abegou           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,16 +27,8 @@ typedef struct s_env
 
 }					t_env;
 
-// custom_libft.c
-size_t				ft_size_cut(char *to_cut);
-char				*ft_cut_env(char *to_cut);
-size_t				arg_len(char **av);
-int					ft_strncmp(const char *s1, const char *s2, size_t n);
-char				*ft_strdup(const char *s);
-size_t				ft_strlen(const char *s);
-
 // lst_tools.c
-void				ft_add_back_env(t_env **lst, t_env *new);
+void				ft_add_back_env(t_env **lst, t_env *new_node);
 void				ft_free_stack_env(t_env *array);
 t_env				*ft_new_env(char *envinfo);
 
@@ -44,5 +36,15 @@ t_env				*ft_new_env(char *envinfo);
 int					ft_pwd(t_env *envinfo);
 int					ft_env(t_env *envinfo, char **av);
 t_env				*init_env(char **envp);
+
+// custom_libft.c
+size_t				ft_size_cut(char *to_cut);
+char				*ft_cut_env(char *to_cut);
+size_t				arg_len(char **av);
+
+// libft.c
+int					ft_strncmp(const char *s1, const char *s2, size_t n);
+char				*ft_strdup(const char *s);
+size_t				ft_strlen(const char *s);
 
 #endif
