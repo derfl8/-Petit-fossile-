057ff27a3a03135fc679131b6e9ad638cc2f1b1f
derfl8 <alfred.begou@gmail.com>
2026-04-26 21:19:18 +0200

    1st try of echo parsing (segfault)

--- builtin/echo.c
+++ builtin/echo.c
@@ -6,15 +6,51 @@
 /*   By: abegou <abegou@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/22 19:41:55 by abegou            #+#    #+#             */
-/*   Updated: 2026/04/22 20:38:15 by abegou           ###   ########.fr       */
+/*   Updated: 2026/04/26 21:18:45 by abegou           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../header/minishell.h"
 
-int ft_echo(char *print)
+bool	ft_parsecho(char *pars)
 {
-    
-    printf("%s", print);
-    return (0);
+	int	i;
+
+	i = 0;
+	if (pars[i] == '-' && pars[i + 1] == 'n')
+	{
+		i++;
+		while (pars[i])
+		{
+			if (pars[i] != 'n' && pars[i])
+				return (false);
+			i++;
+		}
+		return (true);
+	}
+	return (false);
+}
+
+int	ft_echo(char **av)
+{
+	int	i;
+
+	i = 1;
+	while (av[i])
+	{
+		if (ft_parsecho(av[i]) == true)
+		{
+			while (ft_parsecho(av[i]) == true || av[i])
+				i++;
+			while (av[i])
+				printf("%s", av[i++]);
+		}
+		else
+		{
+			while (av[i])
+				printf("%s", av[i++]);
+			printf("\n");
+		}
+	}
+	return (0);
 }
