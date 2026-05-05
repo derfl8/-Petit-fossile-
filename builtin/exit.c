4c9331e2e549433e6d72d9f1c4387d63012cdf95
derfl8 <alfred.begou@gmail.com>
2026-05-05 20:20:19 +0200

    add parsing of exit

--- builtin/exit.c
+++ builtin/exit.c
@@ -6,14 +6,29 @@
 /*   By: abegou <abegou@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/27 18:13:07 by abegou            #+#    #+#             */
-/*   Updated: 2026/04/29 19:23:18 by abegou           ###   ########.fr       */
+/*   Updated: 2026/05/05 16:50:25 by abegou           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../header/builtin.h"
 
-void	ft_exit(t_env *envinfo, int exit_code)
+static int	handle_exit(char **exit_code)
+{
+	// if ()
+	// {
+	// 	printf("bash: exit: %s: numeric argument required\n", exit_code[1]);
+	// 	return (2);
+	// }
+	if (exit_code[1])
+	{
+		printf("bash: exit: too many arguments\n");
+		return (1);
+	}
+	return (0);
+}
+
+void	ft_exit(t_env *envinfo, char **exit_code)
 {
 	ft_free_stack_env(envinfo);
-	exit(exit_code);
+	exit(handle_exit(exit_code));
 }
