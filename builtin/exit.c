d60d7a290d35cf125580e87a85a1c88938e4d3da
derfl8 <alfred.begou@gmail.com>
2026-05-07 16:39:49 +0200

    fix builtin exit

--- builtin/exit.c
+++ builtin/exit.c
@@ -6,48 +6,43 @@
 /*   By: abegou <abegou@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/27 18:13:07 by abegou            #+#    #+#             */
-/*   Updated: 2026/05/06 15:33:42 by abegou           ###   ########.fr       */
+/*   Updated: 2026/05/07 16:37:43 by abegou           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../header/builtin.h"
+
 
 static bool	wich_case(char *exit_code)
 {
 	int	i;
 
 	i = 0;
-	while (exit_code[i])
+	if (exit_code)
 	{
-		if (!ft_isdigit(exit_code[i]))
-			return (false);
-		i++;
+		while (exit_code[i])
+		{
+			if (!ft_isdigit(exit_code[i]))
+				return (false);
+			i++;
+		}
 	}
 	return (true);
 }
 
-static int	handle_exit(char **exit_code)
+void	ft_exit(t_env *envinfo, char **exit_code, int nb_arg)
 {
-	if (exit_code[1])
+	if (nb_arg > 1 && wich_case(exit_code[1]) == false)
 	{
-		if (wich_case(exit_code[1]) == false)
-		{
-			ft_putstr_fd("Petit Fossile: exit: ", 2);
-			ft_putstr_fd(exit_code[1], 2);
-			ft_putendl_fd(": numeric argument required", 2);
-			return (2);
-		}
-		if (exit_code[2])
-		{
-			ft_putendl_fd("Petit Fossile: exit: too many arguments", 2);
-			return (1);
-		}
+		ft_putstr_fd("Petit Fossile: exit: ", 2);
+		ft_putstr_fd(exit_code[1], 2);
+		ft_putendl_fd(": numeric argument required", 2);
 	}
-	return (0);
-}
-
-void	ft_exit(t_env *envinfo, char **exit_code)
-{
+	else if (nb_arg > 2)
+	{
+		ft_putendl_fd("Petit Fossile: exit: too many arguments", 2);
+		return ;
+	}
 	ft_free_stack_env(envinfo);
-	exit(handle_exit(exit_code));
+	exit(2);
 }
