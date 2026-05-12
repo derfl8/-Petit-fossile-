8675565b1a81c297ab4fd57d713e131bddb2c4ae
Alban Decourt-Mesa <aldecour@3E1.42angouleme.fr>
2026-05-12 13:01:12 +0200

    added parser sub functions, needs testing

--- parser.h
+++ parser.h
@@ -6,7 +6,7 @@
 /*   By: aldecour <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/02 22:47:26 by aldecour          #+#    #+#             */
-/*   Updated: 2026/04/22 23:01:28 by aldecour         ###   ########.fr       */
+/*   Updated: 2026/05/12 12:38:33 by aldecour         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,13 +17,16 @@
 #include <stdlib.h>
 #include <libft.h>
 
+# define LEFT 1
+# define RIGHT 2
+
 typedef struct s_lexer
 {
 	const char	*line;
 	size_t		i;
 }				t_lexer;
 
-typedef enum e_type
+typedef enum e_token_type
 {
 	T_PIPE,			// |
 	T_REDIR_IN,		// <
@@ -31,29 +34,45 @@ typedef enum e_type
 	T_HEREDOC,		// <<
 	T_APPEND,		// >>
 	T_WORD,			// basically anything else
-	T_WILDCARD,		// * --MAY NOT BE USED--
+	T_EOF,
 	T_MAX_TOKENS,	// keep as last line
-}	t_type;
+}	t_token_type;
 
 typedef struct s_token
 {
-	t_type	type;
-	char	*value;
-}	t_token;
+	t_token_type	type;
+	char			*value;
+}					t_token;
 
-typedef struct s_parser
+typedef enum e_asl_type
 {
-	t_lexer	lexer;
-	t_token	next_token;
-}			t_parser;
+	ASL_PIPE,
+	ASL_REDIR_IN,
+	ASL_REDIR_OUT,
+	ASL_HEREDOC,
+	ASL_APPEND,
+	ASL_CMD,
+}	t_asl_type;
 
-//placeholder command tree
 typedef struct s_tree
 {
-	t_token			token;
-	struct s_tree	*left;
-	struct s_tree	*right;
+	t_asl_type		type;
+	char			**args;
+	struct s_tree	*next;
 }					t_tree;
 
-t_tree	pf_parser(char *line);
+// FUNCTIONS
+t_token *get_next_token(t_lexer *lexer);
+
+t_tree	**pf_parser(char *line);
+
+// CMD_TREE_UTILS.C
+t_tree	*pf_node_new(void);
+t_tree	*pf_node_last(t_tree *node);
+void	pf_node_add_back(t_tree **cmd_head, t_tree *new);
+
+// PARSER_UTILS.C
+void	parse_cmd(t_token *current, t_tree *cmd_node);
+void	parse_redir(t_token *current, t_tree *cmd_node, t_lexer *lexer);
+void	parse_heredoc(t_token *current, t_tree *cmd_node, t_lexer *lexer);
 #endif
