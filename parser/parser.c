185545ea596f6c9f43a3d01ca95d9223da9e76d6
Alban Decourt-Mesa <aldecour@3E2.42angouleme.fr>
2026-05-19 22:32:23 +0200

    fixed segfault (bad memory allocation for the cmd string)

--- parser/parser.c
+++ parser/parser.c
@@ -6,7 +6,7 @@
 /*   By: abegou <abegou@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/02 22:47:10 by aldecour          #+#    #+#             */
-/*   Updated: 2026/05/12 19:09:10 by aldecour         ###   ########.fr       */
+/*   Updated: 2026/05/17 19:06:19 by aldecour         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,14 +18,12 @@ void	parser_init(t_lexer *lexer, const char *line)
 	lexer->i = 0;
 }
 
-void	parse_logic(t_tree *cmd_current, t_token *next_token, t_lexer *lexer)
+void	parse_special(t_tree *cmd_current, t_token *next_token, t_lexer *lexer)
 {
 	int	err_code;
 
 	err_code = 0;
-	if (next_token->type == T_WORD)
-		parse_cmd(next_token, cmd_current);
-	else if (next_token->type == T_REDIR_IN)
+	if (next_token->type == T_REDIR_IN)
 		parse_redir(next_token, cmd_current, lexer);
 	else if (next_token->type == T_REDIR_OUT)
 		parse_redir(next_token, cmd_current, lexer);
@@ -46,19 +44,19 @@ t_tree	**pf_parser(char *line)
 	t_lexer			lexer;
 
 	cmd_cmd = pf_node_new();
+	cmd_head = &cmd_cmd;
 	parser_init(&lexer, line);
-	cmd_head = NULL;
 	while (1)
 	{
 		next_token = get_next_token(&lexer);
 		if (next_token->type == T_EOF)
 			break ;
 		else if (next_token->type == T_WORD)
-			parse_logic(cmd_cmd, next_token, &lexer);
+			parse_cmd(next_token, cmd_cmd);
 		else
 		{
 			cmd_current = pf_node_new();
-			parse_logic(cmd_current, next_token, &lexer);
+			parse_special(cmd_current, next_token, &lexer);
 			pf_node_add_back(cmd_head, cmd_current);
 		}
 	}
--- parser/parser_utils.c
+++ parser/parser_utils.c
@@ -6,20 +6,41 @@
 /*   By: abegou <abegou@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/12 12:35:42 by aldecour          #+#    #+#             */
-/*   Updated: 2026/05/12 17:07:33 by aldecour         ###   ########.fr       */
+/*   Updated: 2026/05/17 20:33:53 by aldecour         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../header/parser.h"
 
+void	realloc_args(t_tree *node, char *str)
+{
+	int		i;
+
+	i = 0;
+	if (!node->args)
+	{
+		node->args = ft_calloc(2, sizeof(char *));
+		if (!node->args)
+			return ;
+		node->args[i] = ft_calloc(ft_strlen(str) + 1, sizeof(char));
+	}
+	else
+	{
+		while (node->args[i])
+			i++;
+		if (node->args[i + 1] == NULL)
+			node->args[i] = ft_strdup(str);
+	}
+}
+
 void	parse_cmd(t_token *current, t_tree *cmd_node)
 {
 	int		i;
-	char	*arg;
 
 	i = 0;
 	cmd_node->type = ASL_CMD;
-	while (cmd_node->args)
+	realloc_args(cmd_node, current->value);
+	while (cmd_node->args[i][0] == '0')
 		i++;
 	cmd_node->args[i] = current->value;
 }
