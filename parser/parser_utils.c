d19be2267c8e53566b0ad7e9cbdcbfae7ee2eae2
Alban Decourt-Mesa <aldecour@3E1.42angouleme.fr>
2026-05-20 18:12:04 +0200

    fully fixed (inshallah) allocation of dynamic arg table

--- parser/parser_utils.c
+++ parser/parser_utils.c
@@ -6,43 +6,86 @@
 /*   By: abegou <abegou@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/12 12:35:42 by aldecour          #+#    #+#             */
-/*   Updated: 2026/05/17 20:33:53 by aldecour         ###   ########.fr       */
+/*   Updated: 2026/05/20 18:10:49 by aldecour         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../header/parser.h"
 
-void	realloc_args(t_tree *node, char *str)
+size_t	get_args_nbr(char **args)
 {
-	int		i;
+	size_t	i;
 
 	i = 0;
-	if (!node->args)
+	while (args[i])
+		i++;
+	return (i);
+}
+
+void	free_args(char **args, size_t size)
+{
+	size_t	i;
+
+	i = 0;
+	while (i <= size)
 	{
-		node->args = ft_calloc(2, sizeof(char *));
-		if (!node->args)
+		free(args[i]);
+		i++;
+	}
+	free(args);
+}
+
+size_t	double_size_realloc(char ***args, size_t size)
+{
+	char	**tmp;
+	size_t	i;
+
+	i = 0;
+	tmp = ft_calloc(size + 1, sizeof(char *));
+	while ((*args)[i])
+	{
+		tmp[i] = ft_strdup((*args)[i]);
+		if (!tmp[i])
+			return (i);
+		i++;
+	}
+	free_args(*args, size / 2);
+	*args = tmp;
+	return (i - 1);
+}
+
+void	realloc_args(char ***args, t_token *current)
+{
+	static size_t	size;
+	static size_t	i;
+	size_t			check;
+
+	if (!*args)
+	{
+		size = 2;
+		*args = ft_calloc(size + 1, sizeof(char *));
+	}
+	if (!*args)
+		return ;
+	else if (get_args_nbr(*args) == size)
+	{
+		size *= 2;
+		check = double_size_realloc(args, size);
+		if (!(*args)[check])
 			return ;
-		node->args[i] = ft_calloc(ft_strlen(str) + 1, sizeof(char));
 	}
-	else
-	{
-		while (node->args[i])
-			i++;
-		if (node->args[i + 1] == NULL)
-			node->args[i] = ft_strdup(str);
-	}
+	(*args)[i] = ft_calloc(ft_strlen(current->value) + 1, sizeof(char));
+	i++;
 }
 
 void	parse_cmd(t_token *current, t_tree *cmd_node)
 {
-	int		i;
+	static size_t	i;
 
-	i = 0;
 	cmd_node->type = ASL_CMD;
-	realloc_args(cmd_node, current->value);
-	while (cmd_node->args[i][0] == '0')
-		i++;
+	realloc_args(&cmd_node->args, current);
 	cmd_node->args[i] = current->value;
+	i++;
 }
 
 void	parse_redir(t_token *current, t_tree *cmd_node, t_lexer *lexer)
