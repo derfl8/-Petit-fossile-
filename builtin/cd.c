9c87077771f0f21ebf82160462e7e9e50901e31e
derfl8 <alfred.begou@gmail.com>
2026-05-20 17:36:28 +0200

    unset home fix in cd

--- builtin/cd.c
+++ builtin/cd.c
@@ -6,7 +6,7 @@
 /*   By: abegou <abegou@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/15 16:27:14 by abegou            #+#    #+#             */
-/*   Updated: 2026/05/17 19:35:37 by abegou           ###   ########.fr       */
+/*   Updated: 2026/05/20 17:35:29 by abegou           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,16 +18,22 @@ int	ft_cd(t_data *shell, char **av)
 	char	*path;
 
 	tmp = shell->env;
-	if (av[2])
+	if (av[2][1] == 'a')
 	{
 		ft_putendl_fd("Petit Fossile: cd: too many arguments", 2);
 		shell->success_or_failed = 1;
 		return (1);
 	}
-	else if (!av[1] || (av[1][0] == '~' && av[1][1]))
+	else if (!av[1])
 	{
 		while (tmp && ft_strncmp("HOME=", tmp->envinfo, 5) != 0)
 			tmp = tmp->next;
+		if (!tmp)
+		{
+			ft_putendl_fd("Petit Fossile: cd: HOME not set", 2);
+			shell->success_or_failed = 1;
+			return (1);
+		}
 		path = ft_cut_env(tmp->envinfo);
 		chdir(path);
 		free(path);
