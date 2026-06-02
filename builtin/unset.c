858d69ccf4a38c0cb0c957b07d6b26943e36e372
derfl8 <alfred.begou@gmail.com>
2026-06-02 16:49:19 +0200

    UNSET DONE

--- builtin/unset.c
+++ builtin/unset.c
@@ -12,37 +12,37 @@
 
 #include "../header/builtin.h"
 
-static void rm_env(t_data *shell, char *key)
+static void	rm_env(t_data *shell, char *key)
 {
-    t_env   *prev;
-    t_env   *cur;
-    int     s_key;
+	t_env	*prev;
+	t_env	*cur;
+	int		s_key;
 
-    s_key = ft_strlen(key);
-    cur = shell->env;
-    while (ft_strncmp(key, cur->envinfo, s_key) != 0)
-    {
-        prev = cur;
-        cur = cur->next;
-    }
-    free(cur->envinfo);
-    free(cur);
-    return ;
+	s_key = ft_strlen(key);
+	cur = shell->env;
+	while (ft_strncmp(key, cur->envinfo, s_key) != 0)
+	{
+		prev = cur;
+		cur = cur->next;
+	}
+	free(cur->envinfo);
+	free(cur);
+	return ;
 }
 
-int ft_unset(t_data *shell, char **av)
+int	ft_unset(t_data *shell, char **av)
 {
-    size_t  nb_arg;
-    size_t  i;
+	size_t	nb_arg;
+	size_t	i;
 
-    if (!av)
-        return (0);
-    nb_arg = arg_len(av);
-    i = 0;
-    while (i++ != nb_arg)
-    {
-        if (search_env(shell->env, av[i]) == true)
-            rm_env(shell, av[i]);
-    }
-    return (0);
+	if (!av)
+		return (0);
+	nb_arg = arg_len(av);
+	i = 0;
+	while (i++ != nb_arg)
+	{
+		if (search_env(shell->env, av[i]) == true)
+			rm_env(shell, av[i]);
+	}
+	return (0);
 }
