9220f1573a81e3784082a4a689e9fe26d8d7b9a3
derfl8 <alfred.begou@gmail.com>
2026-06-10 16:10:59 +0200

    fix leak exponoarg

--- builtin/export.c
+++ builtin/export.c
@@ -6,7 +6,7 @@
 /*   By: abegou <abegou@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/06/03 19:04:01 by abegou            #+#    #+#             */
-/*   Updated: 2026/06/10 15:07:36 by abegou           ###   ########.fr       */
+/*   Updated: 2026/06/10 16:10:14 by abegou           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ static size_t	ft_size_cut(char *to_cut)
 	size_t	i;
 
 	i = 0;
-	while (to_cut[i] != '=' || to_cut[i])
+	while (to_cut[i] && to_cut[i] != '=')
 		i++;
 	i++;
 	return (i);
@@ -49,16 +49,16 @@ static void	exp_no_arg(t_data *shell)
 	while (tmp)
 	{
 		key = ft_name_var(tmp->envinfo);
-		value = ft_cut_env(tmp->envinfo);
 		if (ft_strchr(tmp->envinfo, '=') != NULL)
 		{
+			value = ft_cut_env(tmp->envinfo);
 			printf("declare -x %s=", key);
 			printf("\"%s\"\n", value);
+			free(value);
 		}
 		else
 			printf("declare -x %s\n", key);
 		free(key);
-		free(value);
 		tmp = tmp->next;
 	}
 }
