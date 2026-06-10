ead4b1473e23e3cdd775ddcad6acc72d422d78a4
derfl8 <alfred.begou@gmail.com>
2026-06-10 15:08:42 +0200

    handle KEY without value in exp_no_arg

--- builtin/export.c
+++ builtin/export.c
@@ -6,19 +6,18 @@
 /*   By: abegou <abegou@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/06/03 19:04:01 by abegou            #+#    #+#             */
-/*   Updated: 2026/06/10 14:00:35 by abegou           ###   ########.fr       */
+/*   Updated: 2026/06/10 15:07:36 by abegou           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../header/builtin.h"
-#include <stdlib.h>
 
 static size_t	ft_size_cut(char *to_cut)
 {
 	size_t	i;
 
 	i = 0;
-	while (to_cut[i] != '=')
+	while (to_cut[i] != '=' || to_cut[i])
 		i++;
 	i++;
 	return (i);
@@ -40,29 +39,34 @@ static char	*ft_name_var(char *var)
 	return (cut);
 }
 
-static void exp_no_arg(t_data *shell)
+static void	exp_no_arg(t_data *shell)
 {
-    char	*key;
+	char	*key;
 	char	*value;
-    t_env	*tmp;
-    
-    tmp = shell->env;
-    while (tmp)
-    {
-        key = ft_name_var(tmp->envinfo);
-        value = ft_cut_env(tmp->envinfo);
-        printf("declare -x %s=", key);
-        printf("\"%s\"\n", value);
-        free(key);
-        free(value);
-        tmp = tmp->next;
-    }
-}    
+	t_env	*tmp;
+
+	tmp = shell->env;
+	while (tmp)
+	{
+		key = ft_name_var(tmp->envinfo);
+		value = ft_cut_env(tmp->envinfo);
+		if (ft_strchr(tmp->envinfo, '=') != NULL)
+		{
+			printf("declare -x %s=", key);
+			printf("\"%s\"\n", value);
+		}
+		else
+			printf("declare -x %s\n", key);
+		free(key);
+		free(value);
+		tmp = tmp->next;
+	}
+}
 
 int	ft_export(t_data *shell, char **av)
 {
 	if (!av[1])
-        exp_no_arg(shell);
+		exp_no_arg(shell);
 	shell->success_or_failed = 0;
 	return (0);
 }
