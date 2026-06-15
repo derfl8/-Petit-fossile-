b4f0690c5333cacbd79b548cf5b387f02f83ed29
Alban Decourt-Mesa <aldecour@1E6.42angouleme.fr>
2026-06-15 21:53:15 +0200

    implemented signal SIGINT with expected behavior

--- signal/signal_handler.c
+++ signal/signal_handler.c
@@ -6,21 +6,34 @@
 /*   By: aldecour <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/06/14 18:50:58 by aldecour          #+#    #+#             */
-/*   Updated: 2026/06/14 21:27:43 by aldecour         ###   ########.fr       */
+/*   Updated: 2026/06/15 21:40:10 by aldecour         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../header/signal_handler.h"
 
+volatile int g_signal_status = 0;
+
 void	signal_handler(int sig)
 {
-	
+	g_signal_status = sig;
+	if (sig == SIGINT)
+	{
+		write(1, "\n", 1);
+		rl_on_new_line();
+		rl_replace_line("", 0);
+		rl_redisplay();
+	}
 }
 
 void	signal_init(void)
 {
 	struct sigaction sa;
 
-	sa.sa_handler = signal_handler;
 	sigemptyset(&sa.sa_mask);
+	sa.sa_flags = 0;
+	sa.sa_handler = signal_handler;
+	sigaction(SIGINT, &sa, NULL);
+	sigaction(SIGQUIT, &sa, NULL);
+	signal(SIGQUIT, SIG_IGN);
 }
