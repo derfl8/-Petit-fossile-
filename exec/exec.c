5ea32051f67ba332d8b1fff7b9e838a58b0f616a
Alban Decourt-Mesa <aldecour@3C3.42angouleme.fr>
2026-06-17 22:11:08 +0200

    fixing some bullcrap

--- exec/exec.c
+++ exec/exec.c
@@ -6,53 +6,56 @@
 /*   By: abegou <abegou@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/06/12 21:27:46 by abegou            #+#    #+#             */
-/*   Updated: 2026/06/17 17:42:43 by abegou           ###   ########.fr       */
+/*   Updated: 2026/06/17 22:10:51 by aldecour         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../header/exec.h"
 
-static void	run_child(t_data *shell, t_tree *tree, char **env)
+ static void    run_child(t_data *shell, t_tree *tree, char **env)
 {
-	char	*bin;
+    char    *bin;
+    char    **exit_code;
 
-	bin = path_verif(shell->env, tree->args[0]);
-	if (bin == NULL)
-	{
-		ft_putstr_fd("Petit Fossile: ", 2);
-		ft_putstr_fd(tree->args[0], 2);
-		ft_putstr_fd(": command not found\n", 2);
-		exit(127);
-	}
-	execve(bin, tree->args, env);
-	free(env);
-	perror(tree->args[0]);
-	exit(1);
+    bin = path_verif(shell->env, tree->args[0]);
+    if (bin == NULL)
+    {
+        exit_code = ft_split("127", '\0');
+        ft_putstr_fd("Petit Fossile: ", 2);
+        ft_putstr_fd(tree->args[0], 2);
+        ft_putstr_fd(": command not found\n", 2);
+        ft_exit(shell, exit_code, tree);
+    }
+    exit_code = ft_split("1", '\0');
+    execve(bin, tree->args, env);
+    free(env);
+    perror(tree->args[0]);
+    ft_exit(shell, exit_code, tree);
 }
 
-void	ft_exec(t_data *shell, t_tree *tree)
+void    ft_exec(t_data *shell, t_tree *tree)
 {
-	int		success;
-	pid_t	pid;
-	int		status;
-	char	**env;
+    int        success;
+    pid_t    pid;
+    int        status;
+    char    **env;
 
-	(void)success;
-	status = 0;
-	if (!tree->args)
-		return ;
-	if (builtin_check(tree->args[0]) == 0)
-	{
-		success = exec_builtin(shell, tree->args, tree);
-		return ;
-	}
-		env = env_to_char(shell);
-	pid = fork();
-	if (pid == 0)
-		run_child(shell, tree, env);
-	else if (pid > 0)
-		waitpid(pid, &status, 0);
-	free_tab(env);
-	shell->success_or_failed = WEXITSTATUS(status);
-	return ;
+    (void)success;
+    status = 0;
+    if (!tree->args)
+        return ;
+    if (builtin_check(tree->args[0]) == 0)
+    {
+        success = exec_builtin(shell, tree->args, tree);
+        return ;
+    }
+        env = env_to_char(shell);
+    pid = fork();
+    if (pid == 0)
+        run_child(shell, tree, env);
+    else if (pid > 0)
+        waitpid(pid, &status, 0);
+    free_tab(env);
+    shell->success_or_failed = WEXITSTATUS(status);
+    return ;
 }
