1a40b245b2660178b648606263a03344e362be44
derfl8 <alfred.begou@gmail.com>
2026-09-11 12:26:51 +0200

    Update expander.c
    wtf try to handle qwe$USER but i need to exit locomotiv

--- expander/expander.c
+++ expander/expander.c
@@ -6,7 +6,7 @@
 /*   By: alfred <alfred@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/09/04 16:09:15 by abegou            #+#    #+#             */
-/*   Updated: 2026/09/11 11:48:43 by alfred           ###   ########.fr       */
+/*   Updated: 2026/09/11 12:24:44 by alfred           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,27 +30,39 @@ void	archaic_expand(t_data *shell, t_tree *tree)
 {
 	int		i;
 	char	*exp_str;
+	char	*tmp;
 
 	i = 0;
 	while (tree)
 	{
 		while (tree->args && tree->args[i])
 		{
-			if (ft_strncmp(tree->args[i], "$?", 3) == 0)
+			if (ft_strchr(tree->args[i], '$') != NULL)
 			{
-				exp_str = ft_itoa(shell->success_or_failed);
-				free(tree->args[i]);
-				tree->args[i] = exp_str;
-			}
-			else if (ft_strncmp(tree->args[i], "$", 1) == 0)
-			{
-				tree->args[i] = ft_strtrim(tree->args[i], "$");
-				if (exp_key_env(shell->env, tree->args[i]) != NULL)
+				tmp = ft_substr(tree->args[i], 0, ft_strchr(tree->args[i], '$') - tree->args[i]);
+				printf("tmp = %s\n", tmp);
+				if (ft_strncmp(tree->args[i], "$?", 3) == 0)
 				{
-					exp_str = exp_key_env(shell->env, tree->args[i]);
+					exp_str = ft_itoa(shell->success_or_failed);
 					free(tree->args[i]);
 					tree->args[i] = exp_str;
 				}
+				else if (ft_strncmp(tree->args[i], "$", 1) == 0)
+				{
+					tree->args[i] = ft_strtrim(tree->args[i], "$");
+					if (exp_key_env(shell->env, tree->args[i]) != NULL)
+					{
+						exp_str = exp_key_env(shell->env, tree->args[i]);
+						free(tree->args[i]);
+						tree->args[i] = exp_str;
+					}
+					else
+					{
+						free(tree->args[i]);
+						tree->args[i] = ft_strdup("");
+					}
+				}
+				tree->args[i] = ft_strjoin(tmp, tree->args[i]);
 			}
 			i++;
 		}
